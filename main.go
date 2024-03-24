3b433a8f8c8a757e8ad0adb9577d69e8b3f7b862
Brian Barasa <briannbig@gmail.com>
2024-03-24 11:43:21 +0300

    Create basic http server with a basic handler function

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-func main()  {
+func main() {
 	fmt.Println("Here we go!")
+	http.HandleFunc("/", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if err != nil {
+		fmt.Println("Error starting http server", err)
+	} else {
+		fmt.Println("Server started")
+	}
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("request body: %d\n", r.Body)
+	io.WriteString(w, "Here we go confirmed!")
+
 }
