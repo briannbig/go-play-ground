a514936d0543605851d214a58e09647f5b451154
Brian Barasa <briannbig@gmail.com>
2024-03-24 11:52:17 +0300

    improve error handling on starting the server

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,11 +14,13 @@ func main() {
 
 	err := http.ListenAndServe(":3333", nil)
 
-	if err != nil {
-		fmt.Println("Error starting http server", err)
-	} else {
-		fmt.Println("Server started")
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server closed")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
 	}
+
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
