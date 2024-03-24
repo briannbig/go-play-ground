f18e9b18297ec3e190527c467c44e858690e78dd
Brian Barasa <briannbig@gmail.com>
2024-03-24 12:40:06 +0300

    utilize http.Server to run multiple servers in the same program

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
+
+const keyServerAddr = "serverAddr"
 
 func main() {
 	fmt.Println("Here we go! a simple http server in golang")
@@ -16,23 +19,60 @@ func main() {
 	mux.HandleFunc("/", getHello)
 	mux.HandleFunc("/greet", greet)
 
-	err := http.ListenAndServe(":3333", mux)
+	ctx, cancelCtx := context.WithCancel(context.Background())
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("Server closed")
-	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
-		os.Exit(1)
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("request body: %d\n", r.Body)
+	ctx := r.Context()
+	fmt.Printf("%s got / request\n", ctx.Value(keyServerAddr))
+
 	io.WriteString(w, "Here we go confirmed!")
 
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s got /greet request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello user")
 }
