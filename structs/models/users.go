e4f6ed1bf60478a6e06343e8aaa3c8d18fe41a70
Brian Barasa <briannbig@gmail.com>
2024-03-25 09:54:14 +0300

    structs: use struct tags to control encoding

--- structs/models/users.go
+++ structs/models/users.go
@@ -7,17 +7,17 @@ import (
 )
 
 type User struct {
-	UserName      string
-	Password      string
-	PreferredFish []string
-	CreatedAt     time.Time
+	UserName      string    `json:"name"`
+	Password      string    `json:"password"`
+	PreferredFish []string  `json:"preferredFish"`
+	CreatedAt     time.Time `json:"createdAt"`
 }
 
 // more like a toString() method in a language like java
 func (u *User) String() string {
 	//return a json representation of the user struct
 	out, err := json.MarshalIndent(u, "", "")
-	
+
 	if err != nil {
 		log.Println(err)
 	}
