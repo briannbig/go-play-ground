3d24664740812b89d443b33e938b0a8aa2e5abe8
briannbig <briannbig@gmail.com>
2025-02-09 22:49:28 +0300

    improve on  goroutine handling and response processing

--- goroutines/main.go
+++ goroutines/main.go
@@ -3,38 +3,50 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-func generateNumbers(total int, ch chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for idx := 1; idx <= total; idx++ {
-
-		fmt.Printf("Sending %d to channel\n", idx)
-		ch <- idx
-	}
-}
-
-func printNumbers(ch <-chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for num := range ch {
-		fmt.Printf("Read %d from channel \n", num)
-	}
-}
-
 func main() {
-	var wg sync.WaitGroup
-	numberChan := make(chan int)
+	respCh := make(chan any, 2)
 
+	start := time.Now()
+	username := fetchUser()
+
+	var wg sync.WaitGroup
 	wg.Add(2)
 
-	go printNumbers(numberChan, &wg)
-	generateNumbers(3, numberChan, &wg)
+	go fetchUserLikes(username, respCh, &wg)
+	go fetchUserMatch(username, respCh, &wg)
 
-	close(numberChan)
-
-	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
-	fmt.Println("Done!")
+	close(respCh)
+
+	for resp := range respCh {
+		fmt.Println("resp: ", resp)
+	}
+
+	fmt.Println("took: ", time.Since(start))
+
+}
+
+func fetchUser() string {
+	time.Sleep(100 * time.Millisecond)
+
+	return "jdoe"
+}
+
+func fetchUserLikes(username string, respCh chan any, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("fetching likes for", username)
+	time.Sleep(150 * time.Millisecond)
+
+	respCh <- 11
+}
+
+func fetchUserMatch(username string, ch chan any, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("fetching match for", username)
+	time.Sleep(125 * time.Millisecond)
+
+	ch <- "jane"
 }
