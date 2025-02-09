a0e748adbe0e74d64be4ef617c5106741c128f2f
briannbig <briannbig@gmail.com>
2025-02-09 21:59:59 +0300

    refactor: update cleanUsername function to return modified context

--- contexts/main.go
+++ contexts/main.go
@@ -18,14 +18,16 @@ func printUsername(ctx context.Context) {
 
 	fmt.Printf("initial %s's value is %s\n><><><><><><cleaning username...\n", keyUsername, ctx.Value(keyUsername))
 
-	cleanUsername(ctx)
+	ctx = cleanUsername(ctx)
 
 	fmt.Printf("Cleaned %s's value is %s\n", keyUsername, ctx.Value(keyUsername))
 
 }
 
-func cleanUsername(ctx context.Context) {
+func cleanUsername(ctx context.Context) context.Context {
 	ctx = context.WithValue(ctx, keyUsername, fmt.Sprintf("~%s", ctx.Value(keyUsername)))
 	fmt.Printf("cleaning %s's value to %s\n", keyUsername, ctx.Value(keyUsername))
+
+	return ctx
 
 }
